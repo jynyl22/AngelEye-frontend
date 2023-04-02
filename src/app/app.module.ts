c6bc499a28316076ed7903e05bf05278240801b6
christy lee <christyjlee07@gmail.com>
2023-04-01 20:17:57 -0500

    draft about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { UploadImageComponent } from './upload-image/upload-image.component';
 import { ProcessImageComponent } from './process-image/process-image.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'upload', component: UploadImageComponent },
-  { path: 'process', component: ProcessImageComponent }
+  { path: 'process', component: ProcessImageComponent },
+  { path: 'about', component: AboutPageComponent}
 ]
 
 @NgModule({
@@ -20,7 +22,8 @@ const routes: Routes = [
     AppComponent,
     HomePageComponent,
     UploadImageComponent,
-    ProcessImageComponent
+    ProcessImageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
