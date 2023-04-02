5d4ec0b6995acb1efbeff333ab32a291cfbe6733
christy lee <christyjlee07@gmail.com>
2023-04-02 07:41:27 -0500

    upload classification

--- src/app/process-image/process-image.component.ts
+++ src/app/process-image/process-image.component.ts
@@ -3,15 +3,21 @@ import { Component } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
+interface JSONResponse {
+  pred: string;
+}
+
 @Component({
   selector: 'app-process-image',
   templateUrl: './process-image.component.html',
   styleUrls: ['./process-image.component.css']
 })
+
 export class ProcessImageComponent {
   public preimageUrl: String = '';
   public safeImageUrl: SafeUrl = '';
   public receivedImage: boolean = false;
+  public prediction: String = '';
 
   constructor(private route: ActivatedRoute, private http: HttpClient, private sanitizer: DomSanitizer) {}
 
@@ -23,5 +29,10 @@ export class ProcessImageComponent {
 
   onProcess() {
     this.receivedImage = true;
+    this.http.get<JSONResponse>('http://localhost:8000/predict').subscribe((data) => {
+      this.prediction = data.pred;
+    });
+
+    console.log(this.prediction);
   }
 }
